165eb63cfd7690213e41a0b576c0626d5135dbf7
Matthew Petruzziello <matthew.petruzziello@botpress.com>
2025-05-21 10:57:08 -0400

    fix(integrations/gcs): unregister log references incorrect integration (#82)

--- integrations/google-chat-spaces/src/setup/unregister.ts
+++ integrations/google-chat-spaces/src/setup/unregister.ts
@@ -1,5 +1,5 @@
 import type { UnregisterFunction } from '../misc/types';
 
 export const unregister: UnregisterFunction = async ({ ctx, client, logger }) => {
-  logger.forBot().info("Unregister process for Zoho Sales IQ HITL integration invoked. No resources to clean up.");
+  logger.forBot().info("Unregister process for Google Chat Spaces integration invoked. No resources to clean up.");
 }
